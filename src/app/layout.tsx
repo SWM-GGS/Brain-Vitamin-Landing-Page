2349365af69b5d0bf1e78a03108ea87cdfc78abe
salmonco <xkqjsslsek80@naver.com>
2023-11-14 19:57:25 +0900

    Refactor: 오픈그래프 태그 추가 #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,16 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "두뇌비타민",
+  title: "경도인지장애 케어 서비스, 두뇌비타민",
   description: "치매예방, 두뇌훈련, 경도인지장애 케어",
+  openGraph: {
+    type: "website",
+    title: "경도인지장애 케어 서비스, 두뇌비타민",
+    description: "치매예방, 두뇌훈련, 경도인지장애 케어",
+    url: "https://brain-vitamin.vercel.app",
+    locale: "ko_KR",
+    images: "https://avatars.githubusercontent.com/u/133128046",
+  },
 };
 
 export default function RootLayout({
