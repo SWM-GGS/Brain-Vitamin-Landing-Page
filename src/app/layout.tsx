3f6f17050323205faeed359229f07d8299ce1557
salmonco <xkqjsslsek80@naver.com>
2023-11-14 20:23:04 +0900

    Chore: 이미지 상대경로로 변경함에 따라 metadataBase url 설정 #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://brain-vitamin.vercel.app"),
   title: "경도인지장애 케어 서비스, 두뇌비타민",
   description: "치매예방, 두뇌훈련, 경도인지장애 케어",
   openGraph: {
@@ -13,7 +14,7 @@ export const metadata: Metadata = {
     description: "치매예방, 두뇌훈련, 경도인지장애 케어",
     url: "https://brain-vitamin.vercel.app",
     locale: "ko_KR",
-    images: "https://avatars.githubusercontent.com/u/133128046",
+    images: "/public/assets/images/logo.png",
   },
 };
 
